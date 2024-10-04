e16ab74661dd5198145a6d587f41de5f09221322
sai kiran <saikiran78911@gmail.com>
2024-10-04 13:04:36 +0530

    Add all the files

---
+++ src/Oct/ex_02102024/Lab002.java
@@ -0,0 +1,8 @@
+package Oct.ex_02102024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("lab002");
+        System.out.println(" just checking the git");
+    }
+}
