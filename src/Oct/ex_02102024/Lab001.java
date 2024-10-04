91135e08eab94033509028ccc4e790278a230056
sai kiran <saikiran78911@gmail.com>
2024-10-04 12:53:31 +0530

    Add all the files

---
+++ src/Oct/ex_02102024/Lab001.java
@@ -0,0 +1,12 @@
+package Oct.ex_02102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+
+
+
+
+}
---
+++ src/Task/Task1.java
@@ -0,0 +1,7 @@
+package Task;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
