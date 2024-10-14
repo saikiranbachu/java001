5f3794faa72fbf92e83ed7c219dd0c61238a7604
sai kiran <saikiran78911@gmail.com>
2024-10-14 22:46:14 +0530

    Add all the files 0f 14th oct

---
+++ src/Oct/ex_02102024/Lab003.java
@@ -0,0 +1,31 @@
+package Oct.ex_02102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        int table = 10 ;
+//        String male = "s";
+//        String female = "s";
+//        System.out.printf("%s weds %s." , male ,female);
+        System.out.println();
+        System.out.printf(" %s * 1 = %s " , table , table*1) ;
+        System.out.println();
+        System.out.printf(" %s * 2 = %s " , table , table*2) ;
+        System.out.println();
+        System.out.printf(" %s * 3 = %s " , table , table*3) ;
+        System.out.println();
+        System.out.printf(" %s * 4 = %s " , table , table*4) ;
+        System.out.println();
+        System.out.printf(" %s * 5 = %s " , table , table*5) ;
+        System.out.println();
+        System.out.printf(" %s * 6 = %s " , table , table*6) ;
+        System.out.println();
+        System.out.printf(" %s * 7 = %s " , table , table*7) ;
+        System.out.println();
+        System.out.printf(" %s * 8 = %s " , table , table*8) ;
+        System.out.println();
+        System.out.printf(" %s * 9 = %s " , table , table*9) ;
+        System.out.println();
+        System.out.printf(" %s * 10 = %s " , table , table*10) ;
+
+    }
+}
