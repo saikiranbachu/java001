11f7d86369c8c9c93813e71beba1594b351dc985
sai kiran <saikiran78911@gmail.com>
2024-10-21 00:48:23 +0530

    Add all the files 0f 15th oct lab

---
+++ src/Oct/ex_02102024/Lab15oct_Increment_Decrement.java
@@ -0,0 +1,19 @@
+package Oct.ex_02102024;
+
+public class Lab15oct_Increment_Decrement {
+    public static void main(String[] args) {
+        // Increment (++) / Decrement (--)
+        // 1) Increment operator
+        //  >> post increment - a++ ( print and increase )
+        //  >> pre increment  - ++a ( increase and print )
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+        //                  11   11    12
+        System.out.println(a);
+
+        // 2)Decrement operator
+        //>> post decrement - a-- ( print and decrese )
+        //>> pre decrement  - --a ( decrese and print )
+    }
+
+}
---
+++ src/Oct/ex_02102024/Lab15oct_conditional_operaters.java
@@ -0,0 +1,41 @@
+package Oct.ex_02102024;
+
+public class Lab15oct_conditional_operaters {
+    public static void main(String[] args) {
+        //Ternary or conditional operaters :
+        //Syntax : RESULT = CONDITION ? EXPRESSION 1 : EXPRESSION 2 ;
+
+        int num1 = 12 ;
+        int num2 = 13 ;
+        String result = num1 > num2 ? "num1 is grater" : "num2 is greater" ;
+        System.out.println(result);
+        int Max = num1 > num2 ? num1 : num2 ;
+        System.out.println(Max);
+
+        //Nested ternary : Ternary in Ternary
+        // Syntax : RESULT = CONDITION1 ? EXPRESSION 1 : (CONDITION2 ? EXPRESSION 2 : EXPRESSION 3)  ;
+
+        int score = 45 ;
+        char grade = (score > 90) ? 'A' : (score > 80 ) ? 'B' : (score > 70 )? 'C' : (score > 60)? 'D' : 'F';
+        System.out.println(grade);
+
+        // TASK : WRITE IT FOR LARGEST OF 3 NUMBERS
+
+        System.out.println("====== TASK =======");
+
+         int no1 = 56 ;
+         int no2 = 234 ;
+         int no3 = 336 ;
+         String largest = (no1 > no2) && (no1 > no3 ) ? "no1":(no2 > no1) && (no2 > no3) ? "no2":"no3";
+         System.out.println("largest no is "+ largest);
+
+         // Increment (++) / Decrement (--)
+        // 1) Increment operator
+        //  >> post increment - a++ ( print and increase )
+        //  >> pre increment  - ++a ( increase and print )
+
+
+
+
+    }
+}
